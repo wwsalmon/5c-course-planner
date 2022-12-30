5e34df7464e537c49121049547ddb6eadd15e8f3
Samson Zhang <wwsamson@yahoo.com>
2022-12-30 16:50:04 -0500

    autogen sem cols

--- components/SemCol.tsx
+++ components/SemCol.tsx
@@ -3,9 +3,9 @@ import BigButton from "./BigButton";
 import UpperH from "./UpperH";
 import classNames from "classnames";
 
-export default function SemCol({children, title, dark}: {children: ReactNode, title: string, dark?: boolean}) {
+export default function SemCol({children, title, dark}: {children?: ReactNode, title: string, dark?: boolean}) {
     return (
-        <div className={classNames("w-72 px-4 border-r overflow-y-auto py-6 flex-shrink-0 border-gray-300", dark && "bg-[#222]")}>
+        <div className={classNames("w-72 px-4 border-r overflow-y-auto py-6 flex-shrink-0", dark ? "border-gray-500" : "border-gray-300", dark && "bg-[#222]")}>
             <h2 className={classNames("mb-8 text-4xl font-light opacity-50", dark && "text-white")}>{title}</h2>
             {children}
             <BigButton className="p-4 mt-6">
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,57 @@ import SemCol from "../components/SemCol";
 import SemClass from "../components/SemClass";
 import SemPos from "../components/SemPos";
 
+let thisYear = new Date().getFullYear();
+let thisMonth = new Date().getMonth() + 1;
+
+if (thisMonth === 12) {
+    thisYear++;
+    thisMonth = 1;
+}
+
+const thisSeason = +(thisMonth > 4);
+const thisSemester = 2 * thisYear + thisSeason;
+const firstSemester = thisSemester - 2;
+const lastSemester = thisSemester + 2;
+
+function decodeSemester(semNum: number) {
+    return (semNum % 2 ? "F" : "S") + (Math.floor(semNum / 2)).toString().substring(2);
+}
+
+console.log(decodeSemester(thisSemester), decodeSemester(firstSemester), decodeSemester(lastSemester));
+
 export default function Home() {
     return (
         <div className="max-w-full overflow-x-auto">
             <div className="flex max-h-screen items-stretch">
-                <SemCol title="F22" dark={true}>
-                    <SemPos title="Main">
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                        <SemClass id="ENGR 084 HM" title="Electronic and Magnet Circuits and Devices" bgColor="#8ae8e9"/>
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                    </SemPos>
-                    <SemPos title="Main">
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                    </SemPos>
-                    <SemPos title="Main">
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                    </SemPos>
-                </SemCol>
-                <SemCol title="S23">
-                    <SemPos title="Main">
-                        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>
-                    </SemPos>
-                </SemCol>
+                {Array(5).fill(0).map((d, i) => firstSemester + i).map(d => (
+                    <SemCol title={decodeSemester(d)} key={d} dark={d < thisSemester}/>
+                ))}
+                {/*<SemCol title="F22" dark={true}>*/}
+                {/*    <SemPos title="Main">*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*        <SemClass id="ENGR 084 HM" title="Electronic and Magnet Circuits and Devices" bgColor="#8ae8e9"/>*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*    </SemPos>*/}
+                {/*    <SemPos title="Main">*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*    </SemPos>*/}
+                {/*    <SemPos title="Main">*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*    </SemPos>*/}
+                {/*</SemCol>*/}
+                {/*<SemCol title="S23">*/}
+                {/*    <SemPos title="Main">*/}
+                {/*        <SemClass id="MS 148 PO" title="Powers of Pleasure" bgColor="#8ae8e9"/>*/}
+                {/*    </SemPos>*/}
+                {/*</SemCol>*/}
             </div>
         </div>
     );
