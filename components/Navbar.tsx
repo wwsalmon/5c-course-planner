658348dc0574c4d080507f6a7d33644d0670aa3b
Samson Zhang <wwsamson@yahoo.com>
2023-01-03 02:27:20 -0500

    fix about page navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import {Dispatch, SetStateAction, useEffect, useState} from "react";
 import {FiSidebar} from "react-icons/fi";
 
-export default function Navbar({isAbout, setIsSidebarOpen, selectedMajors}: {isAbout?: boolean, setIsSidebarOpen?: Dispatch<SetStateAction<boolean>>, selectedMajors: string[]}) {
+export default function Navbar({isAbout, setIsSidebarOpen, selectedMajors}: {isAbout?: boolean, setIsSidebarOpen?: Dispatch<SetStateAction<boolean>>, selectedMajors?: string[]}) {
     return (
         <div className="fixed top-0 left-0 bg-[#222] border-b text-white border-gray-500 w-full h-10 px-4 flex items-center z-10">
             <Link href="/" className="text-sm opacity-50 hover:opacity-100 font-semibold">5C Course Planner</Link>
@@ -11,7 +11,7 @@ export default function Navbar({isAbout, setIsSidebarOpen, selectedMajors}: {isA
             ) : (
                 <Link href="/about" className="ml-auto text-sm opacity-50 hover:opacity-100">About</Link>
             )}
-            {setIsSidebarOpen && (
+            {setIsSidebarOpen && selectedMajors && (
                 <button onClick={() => setIsSidebarOpen(prev => !prev)} className="ml-4 opacity-90 hover:opacity-100 text-sm py-1 px-1 -mr-3 bg-gray-100 text-black flex items-center">
                     <FiSidebar/><span className="ml-2">Major requirements{!!selectedMajors.length && ` (${selectedMajors.length})`}</span>
                     {/*{isSidebarOpen ? ">" : "<"} Requirements menu*/}
